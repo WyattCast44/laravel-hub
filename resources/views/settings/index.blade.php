15059aa0358bd2705dcf76bd0aa97388f7efb918
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-01-07 10:21:15 -0800

    wip

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -4,12 +4,34 @@
 
 <div class="container mx-auto my-16 flex" style="max-width: 1000px">
 
-    <div class="w-3/12 mr-5">
-        Sidebar
+    <div class="w-2/12 mr-5">
+        
+        <nav class="inline-block mt-5">
+
+            <a href="#" class="hover:no-underline flex items-center font-semibold text-red-700 tracking-tight">
+                @svg('user', 'mr-2') Profile
+            </a>
+            
+            <a href="#" class="hover:no-underline flex items-center font-semibold mt-3 tracking-tight text-gray-700 hover:text-red-500">
+                @svg('lock', 'mr-2') Security
+            </a>
+
+            <a href="#" class="hover:no-underline flex items-center font-semibold mt-3 tracking-tight text-gray-700 hover:text-red-500">
+                @svg('tool', 'mr-2') API
+            </a>
+
+        </nav>
+
     </div>
 
     <div class="flex-1">
-        Body
+        
+        <div class="bg-white rounded-lg py-10 px-10 border-solid border shadow">
+    
+            Name
+    
+        </div>
+        
     </div>
 
 </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,8 +2,36 @@
 
 @section('content')
 
-<div class="container mx-auto my-16">
-    {{ $user }}
+<div class="container mx-auto my-16 flex">
+
+    <div class="w-2/12 mr-8">
+        
+        <div class="flex flex-col">
+            <img src="{{ $user->avatar }}" alt="{{ $user->username }}" class="rounded-lg w-full h-auto">
+
+            <h2 class="font-semibold text-2xl my-4">{{$user->username }}</h2>
+
+        </div>
+        
+    </div>
+
+    <div class="flex-1">
+        Content
+    </div>
+
 </div>
 
 @endsection
+
+<!-- 
+
+    We want to list the 
+    - Username
+    - Image
+    - Github profile
+    - Other profiles
+    - repos
+    - templates
+    - favorites?
+
+-->
