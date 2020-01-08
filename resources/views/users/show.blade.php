a341ca73d36ddb3f27abbf40d1f3c8eed3c7228e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-01-08 09:38:29 -0800

    wip

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,17 +6,31 @@
 
     <div class="w-2/12 mr-8">
         
-        <div class="flex flex-col">
-            <img src="{{ $user->avatar }}" alt="{{ $user->username }}" class="rounded-lg w-full h-auto">
+        <div class="flex flex-col items-center">
 
-            <h2 class="font-semibold text-2xl my-4">{{$user->username }}</h2>
+            <img src="{{ $user->avatar }}" alt="{{ $user->username }}" class="rounded-lg w-full h-auto shadow">
+
+            <h2 class="font-semibold text-2xl my-4 text-center leading-tight">
+                {{ $user->name }}
+                <span class="font-light text-xl">{{$user->username }}</span>
+            </h2>
 
         </div>
         
     </div>
 
     <div class="flex-1">
-        Content
+        
+        <div>
+            <nav class="flex mb-8 font-semibold">
+                <a href="#" class="px-4 border-b-2 pb-3 hover:border-red-500 hover:no-underline">Activity</a>
+                <a href="#" class="px-4 border-b-2 pb-3 hover:border-red-500 hover:no-underline">Packages</a>
+                <a href="#" class="px-4 border-b-2 pb-3 hover:border-red-500 hover:no-underline">Templates</a>
+                <a href="#" class="px-4 border-b-2 pb-3 hover:border-red-500 hover:no-underline">Favorites</a>
+                <span class="flex-grow px-4 border-b-2 pb-3"></span>
+            </nav>
+        </div>
+        
     </div>
 
 </div>
