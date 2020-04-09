dbfad8ed52ec720d0563765beab103f79cc9ba3e
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-04-09 01:23:29 -0700

    wip

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container flex mx-auto my-16" style="max-width: 1000px">
+<div class="container flex mx-auto my-4 md:my-16" style="max-width: 1000px">
 
     <div class="flex-1 mx-4">
 
