4cab095b07361a21c9d134b799560ca196154e8b
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-04-09 20:48:31 -0700

    wip

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -4,13 +4,12 @@
 
     <div class="bg-gray-200 border-b">
 
-        <div class="container mx-auto py-16">
+        <div class="container py-16 mx-auto">
 
             <h1 class="text-4xl">Search</h1>
 
         </div>
 
     </div>
-
 
 @endsection
