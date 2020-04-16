ec85e26f3ea886b23c7f4b04a0537f50922a7908
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-04-16 04:16:25 -0700

    wip

--- app/Services/Repo.php
+++ app/Services/Repo.php
@@ -13,6 +13,12 @@ class Repo
 
     public static function fromUrl(string $url)
     {
+        // $valid = $this->client->validateRepoUrl($url);
+
+        // if (!$valid) {
+        //     return null;
+        // }
+
         $parts = explode('/', Str::after($url, 'https://github.com/'));
 
         $vendor = $parts[0];
