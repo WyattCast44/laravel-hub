062ffecb60b60549d797f6dd04a91aef30a077b3
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-05-11 13:16:43 -0700

    wip

--- resources/views/livewire/packages/index.blade.php
+++ resources/views/livewire/packages/index.blade.php
@@ -4,7 +4,7 @@
     <div class="px-4 py-5 bg-white border-b border-gray-200 rounded-t-lg sm:px-6">
         <div class="flex flex-wrap items-center justify-between -mt-2 -ml-4 sm:flex-no-wrap">
             <div class="w-full mt-2 ml-4">
-                <input class="w-full px-3 py-2 leading-tight text-gray-700 border border-gray-300 rounded appearance-none focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Type to search packages... Please '/' to focus..." wire:model="search" role="searchbox" aria-roledescription="search">
+                <input class="w-full px-3 py-2 leading-tight text-gray-700 border border-gray-300 rounded appearance-none focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Type to search packages... Please '/' to focus..." wire:model="search" role="searchbox" aria-roledescription="search" autocomplete="none" spellcheck="false" autofocus>
             </div>
         </div>
     </div>
