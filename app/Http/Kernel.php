dc14b6683da20d1260231069cb7fef6b8fee40b5
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-02-19 20:22:24 -0800

    wip

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \CodeOrange\TurbolinksLocation\TurbolinksLocation::class
+            \Tonysm\TurboLaravel\Http\Middleware\TurboMiddleware::class,
         ],
 
         'api' => [
