8a468296e6bf2db0126b72abcf54d45e44455763
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-03-08 14:32:00 -0800

    wip

--- resources/views/packages/show/index.blade.php
+++ resources/views/packages/show/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('package-page')
 
-    <div class="p-6 md:p-10 markdown-body">
+    <div class="p-6 overflow-scroll prose md:p-10 markdown-body md:overflow-auto prose-red max-w-none">
         @if ($package->parsed_readme == null)
             Readme still parsing
         @else
